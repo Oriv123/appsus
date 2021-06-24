c34db14a268f6d201e8db9a7cb4f9143248b6cb9
Ori Zaken <zaken1ori@gmail.com>
2021-06-24 09:58:59 +0300

    push

--- js/apps/email/cmps/email-preview.js
+++ js/apps/email/cmps/email-preview.js
@@ -2,10 +2,10 @@ export default {
     name: 'email-preview',
     props: ['email'],
     template: `
-        <section class="email-preview" :class="toggleClass" @click = "updateIfRead">
+        <section class="email-preview" :class="toggleClass" @click="updateIfRead">
             <h4>{{email.subject}} 
-                <!-- <span class="email-desc">{{email.body.substring(0, 50)}}... -->
-                <!-- </span> -->
+                <span class="email-desc">{{email.body.substring(0, 50)}}...
+                </span>
             </h4> 
         </section>
         `,
