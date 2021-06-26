d83d90e133b796ca4baf6b569fe0955b57906673
Gilead <gileadgafni4s@gmail.com>
2021-06-26 19:53:57 +0300

    Fixing mail details

--- js/apps/email/cmps/email-details.js
+++ js/apps/email/cmps/email-details.js
@@ -6,24 +6,38 @@ export default {
     template: `
     <section class="email-details" v-if="email">
 
-        <button @click="removeEmail(email.id)">Delete</button>
+       
         
         <h4 class="subject">{{email.subject}}</h4>
         <div class="address-details">
             <p>From: {{email.from}}</p>
             <p>To: {{email.to}}</p>
         </div>
-
+        
         <div class="emailBody" >
             <p>{{email.body}}</p>
         </div>
-
+        
         <p class="details-timestamp">Sent at {{formattedTime}}</p>
-
         <div class="details-button-container">
-            <router-link to="/email">🠔 Back to inbox</router-link>
-            <router-link :to="'/email/compose/'+email.id" class="back">Reply 🠖</router-link>
+        <div class="delete-mail">
+         <button @click="removeEmail(email.id)" title="Delete mail">
+               <i class="fas fa-trash"></i>
+           </button>
+         </div>
+         <section class="bottom-buttons" >
+              <div class="back">
+                <router-link to="/email">
+                🠔 Back
+                </router-link>
         </div>
+         <div class="reply">  
+              <router-link :to="'/email/compose/'+email.id" class="back">
+               <i class="fas fa-reply"></i>
+               </router-link> </div>
+             </section> 
+        
+       </div>
     </section>
     `,
     data() {
@@ -42,7 +56,9 @@ export default {
     },
     computed: {
         formattedTime() {
-            return new Date(this.email.sentAt)
+            const date = new Date(this.email.sentAt).toLocaleDateString();
+            const time = new Date(this.email.sentAt).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
+            return date + ' ' + time;
         }
     },
     created() {
